cfbade40f511eed66406ceeae08bfedd005c7488
Piotr Kubicki <pasjonat@g.pl>
2017-04-12 21:47:19 +0200

    Rebuild

---
+++ Formularz/js/test.js
@@ -0,0 +1,138 @@
+function wypisz()
+{
+
+
+    var odp0=document.getElementById("odp0").value;
+    var odp1=document.getElementById("odp1").value;
+    var odp2=document.getElementById("odp2").value;
+    var odp3=document.getElementById("odp3").value;
+    var odp4=document.getElementById("odp4").value;
+    var odp5=document.getElementById("odp5").value;
+    var odp6=document.getElementById("odp6").value;
+    var odp7=document.getElementById("odp7").value;
+    var odp8=document.getElementById("odp8").value;
+    var odp9=document.getElementById("odp9").value;
+    var odp9=document.getElementById("odp10").value;
+    var odp9=document.getElementById("odp11").value;
+    var odp9=document.getElementById("odp12").value;
+    var odp9=document.getElementById("odp13").value;
+    var odp9=document.getElementById("odp14").value;
+
+    var s = 0;
+    var c = 0;
+    var m = 0;
+    var f = 0;
+
+
+
+        if((odp0=="a")||(odp0=="A")){s++}
+        else if((odp0=="b")||(odp0=="B")){c++}
+        else if((odp0=="c")||(odp0=="C")){m++}
+        else if((odp0=="d")||(odp0=="D")){f++}
+
+        if((odp1=="a")||(odp1=="A")){c++}
+        else if((odp1=="b")||(odp1=="B")){s++}
+        else if((odp1=="c")||(odp1=="C")){f++}
+        else if((odp1=="d")||(odp1=="D")){m++}
+
+        if((odp2=="a")||(odp2=="A")){f++}
+        else if((odp2=="b")||(odp2=="B")){c++}
+        else if((odp2=="c")||(odp2=="C")){s++}
+        else if((odp2=="d")||(odp2=="D")){m++}
+
+        if((odp3=="a")||(odp3=="A")){s++}
+        else if((odp3=="b")||(odp3=="B")){c++}
+        else if((odp3=="c")||(odp3=="C")){m++}
+        else if((odp3=="d")||(odp3=="D")){f++}
+
+        if((odp4=="a")||(odp4=="A")){f++}
+        else if((odp4=="b")||(odp4=="B")){c++}
+        else if((odp4=="c")||(odp4=="C")){m++}
+        else if((odp4=="d")||(odp4=="D")){s++}
+
+        if((odp5=="a")||(odp5=="A")){m++}
+        else if((odp5=="b")||(odp5=="B")){c++}
+        else if((odp5=="c")||(odp5=="C")){s++}
+        else if((odp5=="d")||(odp5=="D")){f++}
+
+        if((odp6=="a")||(odp6=="A")){m++}
+        else if((odp6=="b")||(odp6=="B")){f++}
+        else if((odp6=="c")||(odp6=="C")){c++}
+        else if((odp6=="d")||(odp6=="D")){s++}
+
+        if((odp7=="a")||(odp7=="A")){s++}
+        else if((odp7=="b")||(odp7=="B")){c++}
+        else if((odp7=="c")||(odp7=="C")){m++}
+        else if((odp7=="d")||(odp7=="D")){f++}
+
+        if((odp8=="a")||(odp8=="A")){f++}
+        else if((odp8=="b")||(odp8=="B")){m++}
+        else if((odp8=="c")||(odp8=="C")){c++}
+        else if((odp8=="d")||(odp8=="D")){s++}
+
+        if((odp9=="a")||(odp9=="A")){m++}
+        else if((odp9=="b")||(odp9=="B")){f++}
+        else if((odp9=="c")||(odp9=="C")){s++}
+        else if((odp9=="d")||(odp9=="D")){c++}
+
+        if((odp10=="a")||(odp10=="A")){s++}
+        else if((odp10=="b")||(odp10=="B")){c++}
+        else if((odp10=="c")||(odp10=="C")){m++}
+        else if((odp10=="d")||(odp10=="D")){f++}
+
+        if((odp11=="a")||(odp11=="A")){f++}
+        else if((odp11=="b")||(odp11=="B")){m++}
+        else if((odp11=="c")||(odp11=="C")){s++}
+        else if((odp11=="d")||(odp11=="D")){c++}
+
+        if((odp12=="a")||(odp12=="A")){m++}
+        else if((odp12=="b")||(odp12=="B")){c++}
+        else if((odp12=="c")||(odp12=="C")){f++}
+        else if((odp12=="d")||(odp12=="D")){s++}
+
+        if((odp13=="a")||(odp13=="A")){mc++}
+        else if((odp13=="b")||(odp13=="B")){f++}
+        else if((odp13=="c")||(odp13=="C")){m++}
+        else if((odp13=="d")||(odp13=="D")){s++}
+
+        if((odp14=="a")||(odp14=="A")){s++}
+        else if((odp14=="b")||(odp14=="B")){c++}
+        else if((odp14=="c")||(odp14=="C")){m++}
+        else if((odp14=="d")||(odp14=="D")){f++}
+
+    var n;
+    n = s;
+    if (c>n){n=c}
+    if (m>n){n=m}
+    if (f>n){n=f}
+
+
+    var sangwinik =" To ten typ osoby, którą wszyscy lubią mieć obok siebie. Towarzyski i pogodny, niczym się przesadnie nie przejmuje i wyznaje zasadę „chwytaj dzień”. Mówi żywo, intensywnie gestykulując. Chętnie żartuje i z całego serca śmieje się z żartów innych. Nigdy nie brakuje mu przyjaciół, ba, jego czar jest w stanie zniewolić nawet wrogów. Pod maską jowialnego dowcipnisia kryje się jednak kreatywny umysł, pełen pomysłów i odważnych koncepcji. Siłą napędową sangwinika są emocje. Gdy się do czegoś naprawdę zapali albo kiedy kogoś podziwia, będzie zaangażowany i zaciekawiony. Miewa bowiem problemy z systematycznością i czasem skłonność do zapominania. Żywiołem tradycyjnie przypisywanym sangwinikom jest powietrze.";
+
+    var choleryk =" To typ impulsywny, pełen energii i pasji. Nie znosi stagnacji, nie potrafi usiedzieć spokojnie w miejscu. Łatwo się unosi i wpada w gniew, ale zazwyczaj trwa to krótko i dzieje się pod wpływem chwili. Kiedy mówi, to dobitnie, z zaangażowaniem, często uderzając dłonią o stół, by podkreślić wagę swojej wypowiedzi. Cierpliwość nie jest jego mocną stroną, nie ma też zbyt wiele wyrozumiałości dla słabości innych. Jednak porywa za sobą ludzi swoim nieugiętym dążeniem do celu – wyznaje zasadę, że jeśli coś ma być zrobione, trzeba to zrobić natychmiast. Jest ambitny, praktyczny i dobrze się sprawdza jako lider, ale musi czuć, że jest szanowany i że powierzone mu zadanie odpowiada jego umiejętnościom. Żywiołem tradycyjnie przypisywanym cholerykom jest ogień.";
+
+    var melancholik =" To typ ze skłonnością do introwersji, urodzony idealista. Wszystko mocno przeżywa, wszystkim się przejmuje. Kiedy kocha, to na zabój, kiedy jest szczęśliwy, to do głębi duszy, jednak smutek oznacza dla niego głęboki dół. Woli pracować sam, polega głównie na sobie. Jest też bardzo empatyczny, przejmuje się nieszczęściem, problemami świata, w  odpowiednich warunkach budzi się w nim duch społecznika. Z chęcią poświęci się pomocy tym, których dotknęła jakaś tragedia. Niestety, sam jest skłonny do wpadania w depresję i miewa wahania nastroju. Żywiołem tradycyjnie przypisywanym melancholikom jest ziemia.";
+
+    var flegmatyk =" To typ spokojny, zrównoważony i elastyczny. Łatwo dostosowuje się do każdych warunków, nie cierpi konfliktów i unika ich za wszelką cenę. Zanim podejmie jakąś decyzję, musi starannie przeanalizować wszystkie za i przeciw. Jest znakomitym obserwatorem, któremu nie umknie żaden szczegół. Zazwyczaj wie o ludziach z bliskiego otoczenia więcej,  niż chcieliby zdradzić. Jest wyrozumiały i cierpliwy, nie lubi zmieniać swoich przyzwyczajeń i żyje według własnego rytmu. Kiedy mówi, to spokojnie, cichym głosem, ważąc słowa. Ma dużą skłonność do refleksji i chętnie pogrąża się we własnym świecie. Innych czasami irytuje jego powolność i konserwatyzm. Żywiołem tradycyjnie przypisywanym flegmatykom jest woda.";
+
+
+
+
+    if (n==s)
+    {
+        document.getElementById("pole").innerHTML="<h3>Prawdopodobnie twoim typem osobowości jest Sangwinik :)</h3>"+"<b>Sangwinik</b>:"+"<br />"+sangwinik;
+    }
+    else if (n==c)
+        {
+            document.getElementById("pole").innerHTML="<h3>Prawdopodobnie twoim typem osobowości jest Choleryk :)</h3>"+"<b>Choleryk</b>:"+"<br />"+choleryk;
+        }
+    else if (n==m)
+        {
+            document.getElementById("pole").innerHTML="<h3>Prawdopodobnie twoim typem osobowości jest Melancholik :)</h3>"+"<b>Melancholik</b>:"+"<br />"+melancholik;
+        }
+    else if (n==f)
+        {
+            document.getElementById("pole").innerHTML="<h3>Prawdopodobnie twoim typem osobowości jest Flegmatyk :)</h3>"+"<b>Flegmatyk</b>:"+"<br />"+flegmatyk;
+        }
+
+}
